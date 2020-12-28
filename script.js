8e238a5d523828018b8c7b564c3ce4c982d5b757
askario <abc@abc>
2020-12-28 22:53:17 +0500

    end

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ else if (num2 > num1 && num2 < num3 || num2 < num1 && num2 > num3){
 else if ( num3 > num1 && num3 < num2 || num3 < num1 && num3 > num2){
     alert(num3)
 }
+else {
+    if (isNaN(num1) && isNaN(num2) && isNaN(num3)){
+        alert('вы не ввели число')}
+        else if (isNaN(num1) ){
+            alert('первое число не число')
+        }
+        else if (isNaN(num2)){
+            alert('второе число не число')
+        }
+        else if (isNaN(num3)){
+            alert('третье число не число')
+        }
+        else if ( isNaN(num1) && isNaN(num2) ){
+            alert('первые два числа не числа')
+        }
+        else if ( isNaN(num1) && isNaN(num3) ){
+            alert('первое и последнее числа не числа')
+        }
+        else if ( isNaN(num2) && isNaN(num3) ){
+            alert('последние два числа не числа')
+        }
+    }
