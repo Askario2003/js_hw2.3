c2769e46de261aa8a5a429548154d69c88bf8692
askario <abc@abc>
2020-12-28 22:06:12 +0500

    end

---
+++ script.js
@@ -0,0 +1,13 @@
+var num1 = +prompt('введите первое число')
+var num2 = +prompt('введите второе число')
+var num3 = +prompt('введите третье число')
+4
+if (num1 > num2 && num1 < num3 || num1 > num3 && num1 < num2){
+    alert(num1)
+}
+else if (num2 > num1 && num2 < num3 || num2 < num1 && num2 > num3){
+    alert(num2)
+}
+else if ( num3 > num1 && num3 < num2 || num3 < num1 && num3 > num2){
+    alert(num3)
+}
